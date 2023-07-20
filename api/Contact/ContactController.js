465f7111f8b275bc3a900769dcf24f9842318e69
Sandeep Bisht <>
2023-07-20 13:17:41 +0530

    handle email in contact controller

--- api/Contact/ContactController.js
+++ api/Contact/ContactController.js
@@ -1,19 +1,117 @@
 const connection = require('../../connection');
+const nodemailer = require("nodemailer");
 
 module.exports = {
     createUser : async (req, res) => {
         console.log("inside createTable", req.body);
         const { firstName, lastName, email, phoneNumber, subject, message } = req.body;
 
-        connection.query(`INSERT INTO user VALUES ('${firstName}', '${lastName}','${email}','${phoneNumber}','${subject}','${message}')`, (error, result) =>{
+        connection.query(`INSERT INTO user VALUES ('${firstName}', '${lastName}','${email}','${phoneNumber}','${subject}','${message}')`, async (error, result) =>{
             if(error){
                 console.log(error, "error")
                 return res.status(500).json({ error: "Error in inserting data"});
-            }
-            res.status(200).json({
-                success : 200,
-                 message: "Data Saved" 
-                });
+            } else{
+                let response = await sendEmailViaSmtp(firstName,lastName,phoneNumber, email,subject, message );
+                if (response.messageId) {    
+                    emailSendStatus = true;
+                    res.status(200).json({
+                      emailSendStatus,
+                      message:
+                        "Data Saved Successfully.",
+                    });
+                    }         
+                   else {
+                    emailSendStatus = false;
+                    res.status(200).json({
+                      emailSendStatus,
+                      message:
+                        "Try again.",
+                    });
+                  }
+                
+            }          
         })
+        
     }
+
+    
 }
+
+let sendEmailViaSmtp = async (firstName,lastName,phoneNumber, email,subject, message ) => {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "info.giks@gmail.com",
+          pass: "zapgryhngolaaujr",
+        },
+        tls: { rejectUnauthorized: false },
+      });
+  
+      let info = await transporter.sendMail({
+        from: "info.giks@gmail.com",
+        to: "nitin@giksindia.com",
+        cc: "info.giks@gmail.com",
+        subject: `${subject}`,
+        html: `
+              <div>
+              <P>Hi,<br>                 
+                  <p>
+                  ${firstName} ${lastName} has tried contacting GIKS INDIA.</br>
+                  contact no : ${phoneNumber} </br>
+                  email : ${email}
+                  </P>
+                  <p>${message}</p>
+              </P>             
+             
+           </div>
+           <div>
+              <p>
+              Thanks & Regards,<br>
+                  GIKS INDIA<br>                  
+              </p>
+           </div>
+               `,
+      });
+  
+      if (info.messageId) {
+        let mailToUser = await transporter.sendMail({
+          from: "info.giks@gmail.com",
+          to: email,
+          subject: `${firstName} ${lastName} thanks for contacting GIKS INDIA ✔`,
+          html: `
+              <div>
+              <P>
+                  Dear ${firstName} ${lastName},<br>
+                  <p>
+                  We have recived your request, we will get back to you shortly.
+                  </p>
+                  <p>
+                  Thanks for showing interest in GIKS INDIA
+                  </p>
+              </P>             
+             
+           </div>
+           <div>
+              <p>
+              Thanks & Regards,<br>
+                  GIKS INDIA<br>
+                  Dehradun
+              </p>
+           </div>
+               `
+        }) 
+        if(mailToUser){
+          return info;
+        }        
+      }
+    } catch (err) {
+      return err;
+    }
+  };
+
+
+
+
