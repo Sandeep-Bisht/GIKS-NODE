f16a9ce9d5603cfe7facfc00ba713cad2b419e9f
Deepak Negi <deepak.negi.giksindia.com>
2023-10-30 09:20:00 +0530

    add authentication api

---
+++ api/Authentication/AuthenticationRouting.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const AuthanticationController = require('./AuthenticationController');
+
+router.post("/login", AuthanticationController.isUser)
+
+module.exports = router
