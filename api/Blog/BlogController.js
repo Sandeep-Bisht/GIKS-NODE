58f76b34617e3052d369fd06c849c5fae5de02f8
Deepak Negi <deepak.negi.giksindia.com>
2023-10-31 18:05:03 +0530

    add blog api

--- api/Blog/BlogController.js
+++ api/Blog/BlogController.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports = {
   createBlog: async (req, res) => {
-    const { title, description, content, slug } = req.body;
+    const { title, description, content, slug,category } = req.body;
     console.log(req.body,"check the files")
     const featuredImage = req.files[0]; // Assuming only one image file is uploaded
 
@@ -15,7 +15,7 @@ module.exports = {
       }
       if (results.length === 0) {
         const createTableSql =
-          "CREATE TABLE blog (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(50) NOT NULL, description VARCHAR(200), content VARCHAR(500), slug VARCHAR(50), featuredImage JSON, PRIMARY KEY (id))";
+          "CREATE TABLE blog (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(50) NOT NULL, description VARCHAR(200), content VARCHAR(500), slug VARCHAR(50), category VARCHAR(50), featuredImage JSON, PRIMARY KEY (id))";
         connection.query(createTableSql, (error) => {
           if (error) {
             console.error(error);
@@ -48,13 +48,14 @@ module.exports = {
         };
 
         const insertSql =
-          "INSERT INTO blog (title, description, featuredImage, content, slug) VALUES (?,?,?,?,?)";
+          "INSERT INTO blog (title, description, featuredImage, content, slug, category) VALUES (?,?,?,?,?,?)";
         const values = [
           title,
           description,
           JSON.stringify(imageObject),
           content,
           slug,
+          category,
         ];
         connection.query(insertSql, values, async (error, result) => {
           if (error) {
@@ -119,4 +120,68 @@ module.exports = {
       }
     });
   },
+  updateBlog: async (req, res) => {
+    const { title, description, content, slug, category } = req.body;
+    const featuredImage = req.files[0];
+    console.log(req.body, "check the files");
+    const checkBlogSql = 'SELECT * FROM blog WHERE slug = ?';
+    connection.query(checkBlogSql, [slug], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send("Error checking for the blog post");
+      }
+  
+      if (results.length === 0) {
+        return res.status(404).send("Blog post not found");
+      }
+  
+      function updateBlogData() {
+        fs.readFile(featuredImage.path, (error, data) => {
+          if (error) {
+            console.error(error);
+            return res.status(500).send("Error reading image file");
+          }
+  
+          const imageObject = {
+            fieldname: featuredImage.fieldname,
+            originalname: featuredImage.originalname,
+            encoding: featuredImage.encoding,
+            mimetype: featuredImage.mimetype,
+            destination: featuredImage.destination,
+            filename: featuredImage.filename,
+            path: featuredImage.path,
+            size: featuredImage.size,
+          };
+  
+          const updateSql =
+            "UPDATE blog SET title = ?, description = ?, featuredImage = ?, content = ?, category = ? WHERE slug = ?";
+          const values = [title, description, JSON.stringify(imageObject), content, category, slug];
+  
+          connection.query(updateSql, values, async (error, result) => {
+            if (error) {
+              console.error(error);
+              return res.status(500).send("Error updating blog");
+            }
+            res.status(200).send("Blog post updated successfully");
+          });
+        })
+      }
+  
+      if (featuredImage) {
+        updateBlogData();
+      } else {
+        const updateSql =
+          "UPDATE blog SET title = ?, description = ?, content = ?, category = ? WHERE slug = ?";
+        const values = [title, description, content, category, slug];
+  
+        connection.query(updateSql, values, async (error, result) => {
+          if (error) {
+            console.error(error);
+            return res.status(500).send("Error updating blog");
+          }
+          res.status(200).send("Blog post updated successfully");
+        });
+      }
+    });
+  } 
 };
